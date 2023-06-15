fa9f62ca89650c6eadc29547c5857d8686701e31
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-06-15 18:12:54 +0200

    added mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Olesia:1agSlY9gUgwrYhJ9@cluster0.k8ltqbw.mongodb.net/my-contacts?retryWrites=true&w=majority";
+//1agSlY9gUgwrYhJ9
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
